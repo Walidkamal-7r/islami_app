58779996b6c540b4de3924e7e445ac11b4be96a4
Walid kamal <walidkamal.7r@gmail.com>
2026-08-21 17:14:00 +0300

    + initialize App. + added empty home screen. + added utils.

---
+++ lib/utils/app_assets.dart
@@ -0,0 +1,21 @@
+class AppAssets {
+  static const String quranBg = 'assets/images/quran_bg.png';
+
+  static const String hadethBg = 'assets/images/hadeth_bg.png';
+
+  static const String sebhaBg = 'assets/images/sebha_bg.png';
+
+  static const String radioBg = 'assets/images/radio_bg.png';
+
+  static const String timeBg = 'assets/images/time_bg.png';
+
+  static const String quranIcon = 'assets/images/quran_icon.png';
+
+  static const String hadethIcon = 'assets/images/hadeth_icon.png';
+
+  static const String sebhaIcon = 'assets/images/sebha_icon.png';
+
+  static const String radioIcon = 'assets/images/radio_icon.png';
+
+  static const String timeIcon = 'assets/images/time_icon.png';
+}
