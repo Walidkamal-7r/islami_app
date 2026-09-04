a67ce3f9c9037c9f96c772e488e70f1159360ca8
Walid kamal <walidkamal.7r@gmail.com>
2026-09-04 10:00:14 +0300

    feat: implement most recent suras and make Quran tab fully scrollable
    - Implement Most Recent feature with Provider and shared_preferences
    - Handle edge cases for most recent suras (empty state, persistence)
    - Add Provider and shared_preferences packages
    - Make entire Quran tab scrollable (previously only sura list was scrollable)

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:islami_app/provider/most_recent_provider.dart';
 import 'package:islami_app/ui/screens/home/home_screen.dart';
 import 'package:islami_app/ui/screens/home/tabs/quran/sura_details/sura_details_screen.dart';
 import 'package:islami_app/ui/screens/home/tabs/quran/sura_details1/sura_details_screen1.dart';
 import 'package:islami_app/ui/screens/onboarding/onboarding_screen.dart';
 import 'package:islami_app/utils/app_routes.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(ChangeNotifierProvider(
+      create: (context) => MostRecentProvider(),
+      child: MyApp()
+  )
+  );
 }
 
 class MyApp extends StatelessWidget {
